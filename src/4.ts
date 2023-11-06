f97a241a7e0a1bddb9b5681876ed7b187d8e7675
JuliaChopyk <chopyk.julia@gmail.com>
2023-11-06 18:07:08 +0200

    hw 3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+    private signature: number = Math.random();
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    constructor(private key: Key) {}
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    protected tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.door = false;
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    public comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+      }
+    }
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log("Door open");
+      } else {
+        console.log("Invalid key");
+      }
+    }
+  }
+  
 const key = new Key();
 
 const house = new MyHouse(key);
